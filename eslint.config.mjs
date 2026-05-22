2531c63b97cbeec178e1937e91dff881f1244561
Simon Oskarsson <simon.oskarsson@webflow.com>
2026-05-22 14:16:54 +0200

    FIx eslint issues
    Fix webflow folder path
    Use flat-config `ignores` instead of `ignorePatterns`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  { ignorePatterns: ["**/devlink/**"] },
+  { ignores: ["**/webflow/**"] },
 ];
 
 export default eslintConfig;
